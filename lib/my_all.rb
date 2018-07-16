5350d4d73c72bf8fa83daa7b0bebdeab574264ea
Your Name <you@example.com>
2018-07-16 17:55:50 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
-
+i =0
+#counter
+block_return_values = []
+#array to store values
+while i < collection.length
+#while counter is less than collection.length
+  block_return_values <<
+  yield(collection[i])
+#yield sends (x) as arguement to block of code and add return value to new array
+i+=1
+end
+if block_return_values.include?(false)
+false
+else
+true
+end
 end
